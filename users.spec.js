4be35b077773b777681cb9287017a6cdad1d56ed
Shawn Pocock <sjpocock8@gmail.com>
2023-07-25 10:13:04 -0600

    new testing

--- users.spec.js
+++ users.spec.js
@@ -3,3 +3,23 @@ user = {
     email: 'js@somePretendEmail.com',
     posts: ["this","is","complicated","for","my","tiny brain"]
 }
+describe('First group of tests', () => {
+    test('Jest is working', () => {
+       expect(1).toBe(1);
+    });
+});
+
+describe('Second group of tests', () => {
+    test('returns a random number',()=>{
+        const mockMath = Object.create(global.Math);
+        mockMath.random = jest.fn(()=>0.75);
+        global.Math = mockMath;
+        const id = getNewId();
+        expect(id).toBe(0.75)
+    })
+})
+ 
+describe('Another group of tests', () => {
+    // ...more tests here
+});
+ 
